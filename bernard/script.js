016ed9e55aa32a14317821cee48c39de5358c0e8
SirNeldos <81534510+SirNeldos@users.noreply.github.com>
2022-06-17 08:40:42 +1000

    up

--- bernard/script.js
+++ bernard/script.js
@@ -7,38 +7,38 @@ const theme = {
         'bg': '#fffff1',
         'table': '#e8e8d5',
         'shadow': '#5c4f41',
-        'accent': '#c35d5d',
-        'font': '#1e2d2f'
+        'accent': '#e06c75',
+        'font': '#282c34'
     }, 'dark': {
-        'bg': '#4a4e4c',
+        'bg': '#282c34',
         'table': '#5a605d',
-        'shadow': '#e1d4c6',
-        'accent': '#fd864d',
-        'font': '#fffff1'
+        'shadow': '#c1b1a0',
+        'accent': '#e06c75',
+        'font': '#e1d4c6'
     }
 };
 
-const themeCheck = document.querySelector('#dark-mode input');
-themeCheck.addEventListener('change', () => {
+let dark = false;
+document.querySelector('#dark-mode').addEventListener('click', () => {
     var r = document.querySelector(':root');
 
-    switch (themeCheck.checked) {
-        case true:
-
+    switch (dark) {
+        case false:
             r.style.setProperty('--c-accent', theme.dark.accent);
             r.style.setProperty('--c-background', theme.dark.bg);
             r.style.setProperty('--c-table', theme.dark.table);
             r.style.setProperty('--c-shadow', theme.dark.shadow);
             r.style.setProperty('--c-font', theme.dark.font);
+            dark = true;
             break;
 
-        case false:
-
+        case true:
             r.style.setProperty('--c-accent', theme.light.accent);
             r.style.setProperty('--c-background', theme.light.bg);
             r.style.setProperty('--c-table', theme.light.table);
             r.style.setProperty('--c-shadow', theme.light.shadow);
             r.style.setProperty('--c-font', theme.light.font);
+            dark = false;
             break;
     }
 });
