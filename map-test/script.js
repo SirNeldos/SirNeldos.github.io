476f091dea5e8c6721ccdc7210882011be37a69f
SirNeldos <81534510+SirNeldos@users.noreply.github.com>
2023-03-06 19:21:34 +1100

    Update script.js

--- map-test/script.js
+++ map-test/script.js
@@ -29,44 +29,14 @@ L.imageOverlay(imageUrl, imageBounds).addTo(map);
 // ============ SAVE / LOAD PINS ============== //
 loadPins();
 
-// // load the pins from Local Storage
-// function loadPins() {
-//   if (localStorage.getItem('pins')) {
-//     pins = JSON.parse(localStorage.getItem('pins'));
-
-//     for (let i = 0; i < pins.length; i++) {
-//       const pin = pins[i];
-
-//       var icon = L.divIcon({
-//         html: "<div class='my-icon-label'>" + pin.text + "</div>",
-//         className: 'my-div-icon',
-//         iconAnchor: [10, 30]
-//       });
-
-//       var marker = L.marker(pin.coord, { icon: icon }).addTo(map);
-
-//       marker.on("click", function () {
-//         deleteMarker(i);
-//       });
-//     }
-//   }
-// }
-
 function loadPins() {
   fetch('./data.json')
     .then(response => response.json())
     .then(data => {
 
-      console.log('data:', data);
-
-      //pins = JSON.parse(data);
-
-      //console.log('pins:', pins);
-
       // Do something with the loaded data, such as adding markers to the map
       var count = 0;
       data.forEach(pin => {
-        console.log(pin);
 
         var icon = L.divIcon({
           html: "<div class='my-icon-label'>" + pin.text + "</div>",
@@ -74,12 +44,8 @@ function loadPins() {
           iconAnchor: [10, 30]
         });
 
-        var marker = L.marker(pin.coord, { icon: icon }).addTo(map);
+        L.marker(pin.coord, { icon: icon }).addTo(map);
 
-        marker.on("click", function () {
-          deleteMarker(count);
-        });
-        
         count++;
       });
     })
@@ -89,106 +55,8 @@ function loadPins() {
 }
 
 
-
-// save the pins to Local Storage
-function savePins() {
-  localStorage.setItem('pins', JSON.stringify(pins));
-}
-
-
 // =========== BUTTONS ========== //
 // Center Map Button
 function centerMap() {
   map.setView([mapDim.height / 2, mapDim.width / 2], zoomLevel);
-}
-
-// define a function to add a new pin to the map
-function addPin() {
-  canPin = true;
-}
-
-// Delete Pins
-function deletePin() {
-  canDeletePin = true;
-}
-
-// Download JSON File containing Pins
-function downloadJSON() {
-  // Convert data to JSON string
-  const jsonData = JSON.stringify(pins);
-
-  // Save data to a file
-  const fileName = 'data.json';
-  const fileContent = jsonData;
-  const file = new Blob([fileContent], { type: 'application/json' });
-  const a = document.createElement('a');
-  a.href = URL.createObjectURL(file);
-  a.download = fileName;
-  a.click();
-}
-
-// Log pins in console
-function logPins() {
-  console.log(pins);
-}
-
-
-// ========= ADD PIN ============ //
-map.on('click', function (e) {
-
-  if (canPin) {
-    // prompt the user to enter text for the popup
-    var text = prompt("Enter text for the popup:");
-
-    // create a new pin with a popup
-    var icon = L.divIcon({
-      html: "<div class='my-icon-label'>" + text + "</div>",
-      className: 'my-div-icon',
-      iconAnchor: [10, 30]
-    });
-
-    L.marker(e.latlng, { icon: icon }).addTo(map);
-
-    var pin = { coord: e.latlng, text: text };
-    pins.push(pin);
-    savePins();
-
-    for (let id in map._layers) {
-      map.removeLayer(map._layers[id]);
-    }
-
-    // load in pins from save
-    loadPins();
-
-    // Load map
-    L.imageOverlay(imageUrl, imageBounds).addTo(map);
-  }
-
-  canPin = false;
-});
-
-
-
-// =========== Delete Pin ========== //
-
-function deleteMarker(index) {
-
-  if (canDeletePin) {
-
-    for (let id in map._layers) {
-      map.removeLayer(map._layers[id]);
-    }
-
-    // Remove pin from the saved pins
-    pins.splice(index, 1);
-    savePins();
-
-    // load in pins from save
-    loadPins();
-
-    // Lod map
-    L.imageOverlay(imageUrl, imageBounds).addTo(map);
-  }
-
-  canDeletePin = false;
 }
